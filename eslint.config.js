ee2e15041df3da74212bdbfbe36aa88f34037ce2
stlsgg <gregory.guzey@gmail.com>
2025-10-11 09:02:43 +0300

    feat: add eslint config

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.js"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.browser },
+  },
+]);
