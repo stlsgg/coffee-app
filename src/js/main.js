5afead213669f8438b1634ef5cc0e0cd0d917a10
stlsgg <gregory.guzey@gmail.com>
2025-10-11 10:18:24 +0300

    feat: random button name for each new button state

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,20 @@
-import { updateState, getCurrentTime } from "./utils/index.js";
+import { updateState, getCurrentTime, randomInt } from "./utils/index.js";
 import { renderState } from "./ui/renderState";
 
 let state = {
+  buttonName: "Coffee up!",
   cups: 0,
   lastDrink: "never",
 };
+
+const phrases = [
+  "Cheer up!",
+  "Drink strong coffee!",
+  "Turn on!",
+  "Coffee up!",
+  "Raise adrenaline!",
+  "Squeeze out the grains!",
+];
 
 const coffeeBtn = document.getElementById("coffee-btn");
 const cups = document.getElementById("today-cups");
@@ -15,10 +25,12 @@ renderState(lastCup, state.lastDrink);
 
 coffeeBtn.addEventListener("click", () => {
   state = updateState(state, {
+    buttonName: phrases[randomInt(0, phrases.length - 1)],
     cups: state.cups + 1,
     lastDrink: getCurrentTime(),
   });
 
   renderState(cups, state.cups);
   renderState(lastCup, state.lastDrink);
+  renderState(coffeeBtn, state.buttonName);
 });
